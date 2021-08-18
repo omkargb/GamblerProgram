498751c00a4e64e5f0a8fc9e50f05f4c7ab5561c
omkargb <omkargb107@gmail.com>
2021-08-18 10:24:59 +0530

    [omkargb] Add : checking a gambler win  or not on bet.

--- GamblerProblem/Gambler.cs
+++ GamblerProblem/Gambler.cs
@@ -11,7 +11,25 @@ namespace GamblerProblem
             const int gameStake = 100;
             const int gameBet = 1;
 
-            Console.WriteLine(" Initial values : \t Stake = ${0} \t Bet per game = {1}", gameStake,gameBet);
+            void WinOrLoss()
+            {
+                Random random = new Random();
+                int check = random.Next(0, 2);  // 0 or 1
+
+                if (check == 0)
+                {
+                    Console.WriteLine(" Gambler lost $1.");
+                }
+                else
+                {
+                    Console.WriteLine(" Gambler won $1 .");
+                }
+            }
+
+            Console.WriteLine("\n Initial values : \t Stake = ${0} \t Bet per game = {1}", gameStake,gameBet);
+            Console.WriteLine("\n Making a bet .. Checking Gambler win $1 or not : ");
+            WinOrLoss();
+
         }
     }
 }
