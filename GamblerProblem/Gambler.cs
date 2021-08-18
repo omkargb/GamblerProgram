60ac2914c735ea20cf7f3f2a1f2b4f2dc4f3b1b9
omkargb <omkargb107@gmail.com>
2021-08-18 09:40:32 +0530

    [omkargb] Add : initial values provided to Gambler game

---
+++ GamblerProblem/Gambler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamblerProblem
+{
+    class Gambler
+    {
+        public void Gambling()
+        {
+            const int gameStake = 100;
+            const int gameBet = 1;
+
+            Console.WriteLine(" Initial values : \t Stake = ${0} \t Bet per game = {1}", gameStake,gameBet);
+        }
+    }
+}
---
+++ GamblerProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GamblerProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to gambler problem..");
+            Gambler gambler = new Gambler();
+            gambler.Gambling();
+
+        }
+    }
+}
